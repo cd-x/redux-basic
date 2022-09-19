42253111b2dd08b06b9bc42c8662061b56d47f23
cd-x <whowantsthisemailid@gmail.com>
2022-09-19 19:58:40 +0530

    added auth slice and modified access to existing slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const defaultState = {
+const initialCounterState = {
   counter: 0,
   showCounter: false,
 };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState: defaultState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -24,13 +24,28 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
 /**
  * in case of multiple slice/reducers pass map as value to reducer
  * e.g. reducer: {reducerName1: slice1.reducer, reducerName2: slice2.reducer}
  * and RTK will take care of combinig it
  */
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 export const counterActions = counterSlice.actions;
 export default store;
